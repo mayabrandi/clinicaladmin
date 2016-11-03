c9706c34800c6cbb9cea115a00c37713493e397c
Maya Brandi <mayabrandi@MayaBMBP.scilifelab.se>
2016-11-03 14:54:49 +0100

    config parser fpr python 3XX

--- clinicaladmin/config.py
+++ clinicaladmin/config.py
@@ -1,9 +1,12 @@
-import ConfigParser
+try:
+    import ConfigParser as cp
+except:
+    from six.moves import configparser as cp 
 import os
 import glob
 
 clinical_adminrc=glob.glob(os.path.expanduser('~/.clinical_adminrc'))[0]
-config = ConfigParser.SafeConfigParser()
+config = cp.SafeConfigParser()
 config.readfp(open(clinical_adminrc))
 
 DEMULTIPLEX_DATABASE_URI = config.get('admin data', 'SQLALCHEMY_DATABASE_URI').rstrip()
